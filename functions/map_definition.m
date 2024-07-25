2d6b8d42cbc4cd02d66f970854bdb8b1855d1c0d
MatteoZumerle <matteo.zumerle@virgilio.it>
2024-07-25 17:21:43 +0200

    result fitted

--- functions/map_definition.m
+++ functions/map_definition.m
@@ -1,14 +1,13 @@
-function [obstacles,inflated_obstacles, x_map_size, y_map_size] = map_definition()
+function [obstacles,inflated_obstacles, x_map_size, y_map_size, resolution] = map_definition(select_map)
 
 % Define the walls and obstacles in the map
-
-select_map = 1;
 switch select_map
     case 1
         %Map 1
         % Define max map dimensions
         x_map_size = 10;
         y_map_size = 10;
+        resolution = 0.25;
         obstacles = [0, 0, 10, 0.1;     %
                      0, 0, 0.1, 10;     % these are the walls
                      10, 0, 0.1, 10.1;  %
@@ -28,6 +27,7 @@ switch select_map
         % Define max map dimensions
         x_map_size = 15;
         y_map_size = 10;
+        resolution = 0.1;
          obstacles = [0, 0, 15, 0.1;     %
                      0, 0, 0.1, 10;     % these are the walls
                      15, 0, 0.1, 10.1;  %
@@ -45,6 +45,7 @@ switch select_map
         % Define max map dimensions
         x_map_size = 10;
         y_map_size = 10;
+        resolution = 0.25;
         obstacles = [0, 0, 10, 0.1;     %
                      0, 0, 0.1, 10;     % these are the walls
                      10, 0, 0.1, 10.1;  %
@@ -58,20 +59,19 @@ switch select_map
     case 4
         % Write here your own map
         % .......
-end
+        
+    end
 
 
-% Iinflation factor applied to the obstacles
-inflate = 0.2;
-inflated_obstacles = zeros(size(obstacles));
-for i = 1:height(obstacles)
-    inflated_obstacles(i, 1) = obstacles(i, 1) - inflate; % x iniziale spostato a sinistra
-    inflated_obstacles(i, 2) = obstacles(i, 2) - inflate; % y iniziale spostato in basso
-    inflated_obstacles(i, 3) = obstacles(i, 3) + 2*inflate; % larghezza aumentata
-    inflated_obstacles(i, 4) = obstacles(i, 4) + 2*inflate; % altezza aumentata
-end
+    % Iinflation factor applied to the obstacles
+    inflate = 0.2;
+    inflated_obstacles = zeros(size(obstacles));
+    for i = 1:height(obstacles)
+        inflated_obstacles(i, 1) = obstacles(i, 1) - inflate; % x iniziale spostato a sinistra
+        inflated_obstacles(i, 2) = obstacles(i, 2) - inflate; % y iniziale spostato in basso
+        inflated_obstacles(i, 3) = obstacles(i, 3) + 2*inflate; % larghezza aumentata
+        inflated_obstacles(i, 4) = obstacles(i, 4) + 2*inflate; % altezza aumentata
+    end
 
-% Map 3
-% ...
 end
 
